f973d27dc0aa41168cc1d7090bf8478982a8c2c3
HTMLissa <melissa_w1994@yahoo.de>
2022-07-29 16:28:28 -0500

    creating four functions to add, subtract, multiply, and divide two numbers

---
+++ app.js
@@ -0,0 +1,21 @@
+// Calculator functions
+
+// add
+function add(x, y) {
+  return x + y;
+}
+
+// subtract
+function subtract(x, y) {
+  return x - y;
+}
+
+// multiply
+function multiply(x, y) {
+  return x * y;
+}
+
+// divide
+function divide(x, y) {
+  return x / y;
+}
