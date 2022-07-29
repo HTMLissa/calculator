acffc8b30c7f5e983103614aaac1b4bacd74c09a
HTMLissa <melissa_w1994@yahoo.de>
2022-07-29 16:33:39 -0500

    creating a function operate that takes an operator and 2 numbers and then calls one of the  calculator functions on the numbers

--- app.js
+++ app.js
@@ -19,3 +19,10 @@ function multiply(x, y) {
 function divide(x, y) {
   return x / y;
 }
+
+// Create a new function "operate" that takes an operator and 2 numbers and then calls one of the above functions on the numbers
+
+function operate(operator, num1, num2) {
+  let result = operator(num1, num2);
+  return result;
+}
