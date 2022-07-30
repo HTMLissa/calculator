a8b3cc80c2363f8f1c0bac0db78ebee83c314370
HTMLissa <melissa_w1994@yahoo.de>
2022-07-29 21:15:52 -0500

    adding basic functionality to the calculator

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 // Calculator functions
 
 // add
-function add(x, y) {
+let add = function (x, y) {
   return x + y;
-}
+};
 
 // subtract
-function subtract(x, y) {
+let subtract = function (x, y) {
   return x - y;
-}
+};
 
 // multiply
-function multiply(x, y) {
+let multiply = function (x, y) {
   return x * y;
-}
+};
 
 // divide
-function divide(x, y) {
+let divide = function (x, y) {
   return x / y;
-}
+};
+
+let value1, value2, chosenOperator, result;
 
 // Create a new function "operate" that takes an operator and 2 numbers and then calls one of the above functions on the numbers
 
@@ -29,10 +31,36 @@ function operate(operator, num1, num2) {
 
 // Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step.
 let numberButtons = document.querySelectorAll(".numeric");
-let result = document.querySelector(".result");
+let resultDisplay = document.querySelector(".result");
 
 numberButtons.forEach((num) => {
   num.addEventListener("click", function () {
-    result.textContent += num.getAttribute("data-value");
+    resultDisplay.textContent += num.getAttribute("data-value");
   });
+});
+
+// Make the calculator work!
+let operatorButtons = document.querySelectorAll(".operator");
+let equalButton = document.querySelector(".equal");
+
+operatorButtons.forEach((operator) => {
+  operator.addEventListener("click", function () {
+    value1 = parseInt(resultDisplay.textContent);
+    chosenOperator = operator.getAttribute("data-value");
+    resultDisplay.textContent = "";
+  });
+});
+
+equalButton.addEventListener("click", function () {
+  value2 = parseInt(resultDisplay.textContent);
+  if (chosenOperator == "add") {
+    result = operate(add, value1, value2);
+  } else if (chosenOperator == "subtract") {
+    result = operate(subtract, value1, value2);
+  } else if (chosenOperator == "multiply") {
+    result = operate(multiply, value1, value2);
+  } else if (chosenOperator == "divide") {
+    result = operate(divide, value1, value2);
+  }
+  resultDisplay.textContent = result;
 });
