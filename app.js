ec321f3182e2020997cfeb9faa84236d93d6bbef
HTMLissa <melissa_w1994@yahoo.de>
2022-08-10 17:14:03 -0500

    adding functionality to the power button

--- app.js
+++ app.js
@@ -23,14 +23,14 @@ let divide = function (x, y) {
 let value1, chosenOperator;
 let result = 0;
 
-// Create a new function "operate" that takes an operator and 2 numbers and then calls one of the above functions on the numbers
+// Creating a new function "operate" that takes an operator and 2 numbers and then calls one of the above functions on the numbers
 
 function operate(operator, num1, num2) {
   let result = operator(num1, num2);
   return result;
 }
 
-// Create a functions that populate the display when you click the number buttons
+// Creating a function that populate the display when you click the number buttons
 
 let numberButtons = document.querySelectorAll(".numeric");
 let resultDisplay = document.querySelector(".result");
@@ -45,7 +45,7 @@ numberButtons.forEach((num) => {
   });
 });
 
-// Perform an operation selected by the user and display the result rounded to 7 decimal places (using .toFixed)
+// Performing an operation selected by the user and display the result rounded to 7 decimal places (using .toFixed)
 
 function calculate() {
   if (chosenOperator == "add") {
@@ -65,7 +65,7 @@ function calculate() {
   return result;
 }
 
-// Add functionality to the operator buttons
+// Adding functionality to the operator buttons
 
 let operatorButtons = document.querySelectorAll(".operator");
 let equalButton = document.querySelector(".equal");
@@ -89,29 +89,68 @@ equalButton.addEventListener("click", function () {
   resultDisplay.textContent = result;
 });
 
-// Add functionality to clear calculator display
+// Adding functionality to clear calculator display
 
 let clearButton = document.querySelector(".clear-Btn");
 
 clearButton.addEventListener("click", function () {
+  clearAll();
+});
+
+function clearAll() {
   result = 0;
   chosenOperator = undefined;
   resultDisplay.textContent = "";
   decimalPointButton.style.pointerEvents = "auto";
-});
+}
 
-// Add functionality to delete input made by the user
+// Adding functionality to delete input made by the user
 let deleteButton = document.querySelector(".delete-Btn");
 
 deleteButton.addEventListener("click", function () {
   resultDisplay.textContent = resultDisplay.textContent.slice(0, -1);
 });
 
-// Add functionality to decimal point button
+// Adding functionality to decimal point button
 let decimalPointButton = document.querySelector(".decimal-point");
-let decimalButtonPushed = false;
 
 decimalPointButton.addEventListener("click", function () {
   resultDisplay.textContent += decimalPointButton.getAttribute("data-value");
   decimalPointButton.style.pointerEvents = "none";
 });
+
+// Adding functionality to on/off button
+let powerButton = document.querySelector(".power-Btn");
+let displayScreen = document.querySelector(".screen");
+let allButtons = document.querySelectorAll(".col");
+let powerOn = false;
+disableButtons();
+
+powerButton.addEventListener("click", function () {
+  if (!powerOn) {
+    displayScreen.style.backgroundColor = "#87b37a";
+    enableButtons();
+    powerOn = true;
+  } else {
+    displayScreen.style.backgroundColor = "#77867f";
+    disableButtons();
+    powerOn = false;
+  }
+});
+
+function disableButtons() {
+  allButtons.forEach((button) => {
+    button.style.pointerEvents = "none";
+    button.style.opacity = ".4";
+  });
+  powerButton.style.pointerEvents = "auto";
+  powerButton.style.opacity = "1";
+  clearAll();
+}
+
+function enableButtons() {
+  allButtons.forEach((button) => {
+    button.style.pointerEvents = "auto";
+    button.style.opacity = "1";
+  });
+}
